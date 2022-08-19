56fc17cfb50d04235b1f3782955556732da8240a
kityandhero <kityandhero@126.com>
2022-08-19 14:30:20 +0800

    feature: update

--- src/infrastructure/EasySoft.Core.GeneralLogTransmitter/MessageQuery/Query.cs
+++ src/infrastructure/EasySoft.Core.GeneralLogTransmitter/MessageQuery/Query.cs
@@ -4,9 +4,6 @@ using EasySoft.Core.RabbitMQ;
 
 namespace EasySoft.Core.GeneralLogTransmitter.MessageQuery
 {
-    /// <summary>
-    /// 库存变更队列
-    /// </summary>
     public class Query : BasicQuery<IGeneralLogExchange>
     {
         protected override string RouteName => "GeneralLogRoute";
