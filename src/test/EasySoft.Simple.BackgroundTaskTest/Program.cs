dbe633d97e99a44ed35f109da005df04c635ecb2
kityandhero <kityandhero@126.com>
2022-09-17 18:57:16 +0800

    feature: update

---
+++ src/test/EasySoft.Simple.BackgroundTaskTest/Program.cs
@@ -0,0 +1,7 @@
+using EasySoft.Simple.BackgroundTaskTest;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services => { services.AddHostedService<Worker>(); })
+    .Build();
+
+await host.RunAsync();
