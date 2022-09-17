884c0134551d3a4f65f6c241d9dfb97b5120145e
kityandhero <kityandhero@126.com>
2022-09-17 09:54:24 +0800

    feature: update

---
+++ src/infrastructure/EasySoft.Core.FeatureManagement/ExtensionMethods/ServiceCollectionExtensions.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.FeatureManagement;
+
+namespace EasySoft.Core.FeatureManagement.ExtensionMethods;
+
+public static class ServiceCollectionExtensions
+{
+    /// <summary>
+    /// Feature Management
+    /// </summary>  
+    /// <remarks>
+    /// It is suitable for the scenario where the switch function is required
+    /// </remarks>
+    public static IServiceCollection AddAdvanceFeatureManagement(
+        this IServiceCollection serviceCollection,
+        Action<IFeatureManagementBuilder>? action = null
+    )
+    {
+        var builder = serviceCollection.AddFeatureManagement();
+
+        action?.Invoke(builder);
+
+        return serviceCollection;
+    }
+
+    /// <summary>
+    /// Feature Management
+    /// </summary>  
+    /// <remarks>
+    /// It is suitable for the scenario where the switch function is required
+    /// </remarks>
+    public static IServiceCollection AddAdvanceFeatureManagement(
+        this IServiceCollection serviceCollection,
+        IConfiguration configuration,
+        Action<IFeatureManagementBuilder>? action = null
+    )
+    {
+        var builder = serviceCollection.AddFeatureManagement(configuration);
+
+        action?.Invoke(builder);
+
+        return serviceCollection;
+    }
+}
---
+++ src/infrastructure/EasySoft.Core.FeatureManagement/ExtensionMethods/WebApplicationBuilderExtensions.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.FeatureManagement;
+
+namespace EasySoft.Core.FeatureManagement.ExtensionMethods;
+
+/// <summary>
+/// Feature Management
+/// </summary>  
+/// <remarks>
+/// It is suitable for the scenario where the switch function is required
+/// </remarks>
+public static class WebApplicationBuilderExtensions
+{
+    /// <summary>
+    /// Feature Management
+    /// </summary>  
+    /// <remarks>
+    /// It is suitable for the scenario where the switch function is required
+    /// </remarks>
+    public static WebApplicationBuilder AddAdvanceFeatureManagement(
+        this WebApplicationBuilder builder,
+        Action<IFeatureManagementBuilder>? action = null
+    )
+    {
+        builder.Services.AddAdvanceFeatureManagement(action);
+
+        return builder;
+    }
+
+    /// <summary>
+    /// Feature Management
+    /// </summary>  
+    /// <remarks>
+    /// It is suitable for the scenario where the switch function is required
+    /// </remarks>
+    public static WebApplicationBuilder AddAdvanceFeatureManagement(
+        this WebApplicationBuilder builder,
+        IConfiguration configuration,
+        Action<IFeatureManagementBuilder>? action = null
+    )
+    {
+        builder.Services.AddAdvanceFeatureManagement(configuration, action);
+
+        return builder;
+    }
+}
